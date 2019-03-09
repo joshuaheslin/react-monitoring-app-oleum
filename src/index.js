570fec43898e3446e2bf70e09d887063b3e6f42c
Joshua <josh@kas.com.au>
2019-03-09 19:18:39 +1000

    message

--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
 
 ReactDOM.render(
   <BrowserRouter>
---
+++ src/readme.txt
@@ -0,0 +1,2 @@
+Setting buildpack to https://github.com/mars/create-react-app-buildpack.git... done
+https://hidden-fjord-49077.herokuapp.com/ | https://git.heroku.com/hidden-fjord-49077.git
